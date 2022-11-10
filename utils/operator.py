8394facdfe9d8fbd1ec934ccc56819a2731e18fb
mrvanolog <mrvanolog97@gmail.com>
2022-11-10 15:36:40 +0000

    Update verbosity logic and add new methods

--- utils/operator.py
+++ utils/operator.py
@@ -12,8 +12,7 @@ from models.protocnn import ProtCNN
 class Operator():
     """Utility class that operates initialization, training and evaluation of the model.
     """
-    def __init__(self, verbose: bool=False):
-        self.verbose = verbose
+    def __init__(self):
         self.data_dir: str = './random_split'
 
         # hyperparameters
@@ -32,12 +31,14 @@ class Operator():
         self.dev_dataset: data.Dataset = None
         self.test_dataset: data.Dataset = None
         self.dataloaders: Dict[data.DataLoader] = None
-        self.prot_cnn: ProtCNN = None
+        self.prot_cnn: pl.LightningModule = None
         self.trainer: pl.Trainer = None
 
         self.init_status: bool = False
+        self.train_status: bool = False
 
-    def init(self, seq_max_len: int, batch_size: int, num_workers: int):
+    def init(self, seq_max_len: int, batch_size: int,
+             num_workers: int, verbose: bool=False, **kwargs):
         # set hyperparameters
         self.seq_max_len = seq_max_len
         self.batch_size = batch_size
@@ -47,7 +48,7 @@ class Operator():
         self.fam2label = build_labels(self.train_targets)
         self.word2id = build_vocab(self.train_data)
         self.num_classes = len(self.fam2label)
-        if self.verbose:
+        if verbose:
             print(f'AA dictionary formed. The length of dictionary is: {len(self.word2id)}.')
 
         self.train_dataset = SequenceDataset(self.word2id, self.fam2label,
@@ -56,6 +57,8 @@ class Operator():
                                            seq_max_len, self.data_dir, 'dev')
         self.test_dataset = SequenceDataset(self.word2id, self.fam2label,
                                             seq_max_len, self.data_dir, 'test')
+        if verbose:
+            print('Datasets created')
 
         self.dataloaders = {}
         self.dataloaders['train'] = data.DataLoader(
@@ -76,10 +79,14 @@ class Operator():
             shuffle=False,
             num_workers=num_workers,
         )
+        if verbose:
+            print('Dataloaders created')
 
         self.init_status = True
+        if verbose:
+            print('Initialization complete')
 
-    def train(self, gpus: int, epochs: int):
+    def train(self, gpus: int, epochs: int, **kwargs):
         if not self.init_status:
             print('error: pfam must be initialized before training, use <pfam init>')
             return
@@ -93,8 +100,16 @@ class Operator():
         self.trainer = pl.Trainer(gpus=gpus, max_epochs=epochs)
         self.trainer.fit(self.prot_cnn, self.dataloaders['train'], self.dataloaders['dev'])
 
-    def evaluate(self):
-        pass
+        self.train_status = True
 
-    def test(self):
-        pass
+    def test(self, verbose: bool=False, **kwargs):
+        if not self.train_status:
+            print('error: pfam must be trained before testing, use <pfam train>')
+            return
+
+        self.trainer.test(self.prot_cnn, self.dataloaders['test'], verbose=verbose)
+
+    def predict(self, verbose: bool=False, **kwargs):
+        if not self.train_status:
+            print('error: pfam must be trained before running inference, use <pfam train>')
+            return
